c04e7e0d651ec08959251e3a882d475746155e15
tekuno3 <tekuteku777777@gmail.com>
2024-11-09 00:30:19 +0000

    Stack追加

--- src/Components/Stack.tsx
+++ src/Components/Stack.tsx
@@ -1,14 +1,49 @@
-import { ComponentPropsWithoutRef } from "react";
+import { Children, ComponentPropsWithoutRef, Fragment, ReactNode } from "react";
 
 export interface StackProps extends ComponentPropsWithoutRef<'div'> {
+    children?: ReactNode,
     direction?: 'horizontal' | 'vertical',
-    spacing?: ''
+    divider?: ReactNode
+    spacing?: number | string,
+}
+
+export const stackClasses = {
+    root: 'stack-root',
 }
 
 export function Stack({
-    ...rest,
+    children,
+    direction = 'vertical',
+    divider,
+    spacing,
+    ...rest
 }: StackProps) {
+    const flexDirection = ({
+        horizontal: 'row',
+        vertical: 'column',
+    } as const)[direction];
+
+    const gapDirection = ({
+        horizontal: 'column',
+        vertical: 'row',
+    } as const)[direction];
+
     return (
-        <></>
+        <div
+            className={stackClasses.root}
+            css={{
+                display: 'flex',
+                flexDirection: flexDirection,
+                [`${gapDirection}Gap`]: spacing,
+            }}
+            {...rest}
+        >
+            {Children.map(children, (child, index) => (
+                <Fragment key={index}>
+                    {child}
+                    {divider}
+                </Fragment>
+            ))}
+        </div>
     );
 }
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,11 +1,19 @@
+import { Stack } from "@src/Components/Stack";
+
 export interface HomeProps {
 
 }
 
 export function Home({ }: HomeProps) {
     return (
-        <div css={{ width: '100%', }}>
-            オホ
-        </div>
+        <Stack
+            direction="vertical"
+            spacing={2}
+            css={{ height: '100vh', width: '100%', }}
+        >
+            <div css={{ backgroundColor: 'gray', height: '200px', width: '200px' }}>1</div>
+            <div css={{ backgroundColor: 'gray', height: '200px', width: '200px' }}>2</div>
+            <div css={{ backgroundColor: 'gray', minHeight: '200px', width: '200px', flexGrow: 1, }}>3</div>
+        </Stack>
     );
 }
